e137b7e0b3b256c0066664bd7ed98de23dd7e4cb
Arnadi Denanda <arz1893@gmail.com>
2018-03-14 15:21:23 +0700

    Updated on March 14 2018

--- public/js/vue/vue_customer.js
+++ public/js/vue/vue_customer.js
@@ -19,27 +19,40 @@ var modal_add_customer = new Vue({
     methods: {
 
         submitCustomer: function () {
+            var vm = this;
             this.$validator.validateAll().then((result) => {
                 if (result) {
                     axios.post(window.location.protocol + "//" + window.location.host + "/" + 'customer',this.customer)
-                        .then(response => {
-                        // console.log(response);
-                        // console.log(response.data.systemId, response.data.name, response.data.phone);
-                        var option = new Option(response.data.name + ' - ' + response.data.phone, response.data.systemId, false, false);
-                        $('#customerId').append(option ).trigger('change');
+                            .then(response => {
+                            // console.log(response);
+                            // console.log(response.data.systemId, response.data.name, response.data.phone);
+                            var option = new Option(response.data.name + ' - ' + response.data.phone, response.data.systemId, false, false);
+                            $('#customerId').append(option).trigger('change');
+                            $('#customerId').val(response.data.systemId).trigger('change');
 
-                        if(Root.is_anonymous != null) {
-                            Root.is_anonymous = false;
-                        }
-                        if(Root.is_customer != null) {
-                            Root.is_customer = true;
-                        }
-                        $('#modal_add_customer').modal('hide');
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-                    // eslint-disable-next-line
+                            vm.customer.name = '';
+                            vm.customer.gender = '';
+                            vm.customer.phone = '';
+                            vm.customer.birthdate = '';
+                            vm.customer.email = '';
+                            vm.customer.address = '';
+                            vm.customer.nation = '';
+                            vm.customer.city = '';
+                            vm.customer.memo = '';
+
+                            if(Root.is_anonymous != null) {
+                                Root.is_anonymous = false;
+                            }
+                            if(Root.is_customer != null) {
+                                Root.is_customer = true;
+                            }
+                            $('#modal_add_customer').modal('hide');
+                            this.errors.clear();
+                            })
+                        .catch(error => {
+                            console.log(error);
+                        });
+
                 }
             });
         }
